5e2a574d8dfb0a97cd5c32542bfa82339e24ecf6
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-06-27 23:57:14 +0300

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,60 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from "react";
+import Section from './Section/Section';
+import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
+import Statistics from "./Statistics/Statistics";
+import Notification from "./Notification/Notification.jsx";
+
+
+export class  App extends React.Component {
+  state={
+    good:0,
+    natural:0,
+    bad:0,
+  };
+
+  onBtnClick = feedback =>{
+    this.setState(prevSate =>({
+      [feedback]:prevSate[feedback] + 1,
+    }));
+  };
+
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    const total = this.countTotalFeedback();
+    return Math.round((good * 100) / total);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onBtnClick={this.onBtnClick}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
+      </div>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types'; 
+
+import st from 'components/FeedbackOptions/FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onBtnClick }) => (
+    <div>
+      {options.map(option => (
+        <button className={st.Button} type="button" key={option} onClick={() => onBtnClick(option)}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+  
+  FeedbackOptions.propTypes = {
+    options: PropTypes.array.isRequired,
+    onBtnClick: PropTypes.func.isRequired,
+  };
+
+
+
+export default FeedbackOptions;
+
