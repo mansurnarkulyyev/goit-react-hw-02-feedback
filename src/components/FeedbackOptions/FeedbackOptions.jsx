2a5883da7374cf097f9fea0f78912aad6b48a465
Mansur Narkulyyev <50501517+mansurnarkulyyev@users.noreply.github.com>
2022-07-02 17:43:59 +0300

    fixed

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import PropTypes from 'prop-types'; 
+import PropTypes from 'prop-types';
 
 import st from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onBtnClick }) => (
-    <div>
-      {options.map(option => (
-        <button className={st.Button} type="button" key={option} onClick={() => onBtnClick(option)}>
-          {option}
-        </button>
-      ))}
-    </div>
-  );
-  
-  FeedbackOptions.propTypes = {
-    options: PropTypes.array.isRequired,
-    onBtnClick: PropTypes.func.isRequired,
-  };
+  <div>
+    {options.map(option => (
+      <button className={st.Button} type="button" key={option} onClick={() => onBtnClick(option)}>
+        {option}
+      </button>
+    ))}
+  </div>
+);
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.shape({
+    good: PropTypes.number.isRequired,
+    neutral: PropTypes.number.isRequired,
+    bad: PropTypes.number.isRequired,
+  }),
+  onBtnClick: PropTypes.func.isRequired,
+};
 
 
 
